2e606338c63d6f8e89a6e4567a422534274fa28d
paul <me@me.com>
2015-06-11 21:03:01 +0100

    added enemy group basics

--- game.js
+++ game.js
@@ -12,7 +12,9 @@ var game = new Phaser.Game(800,600,Phaser.Auto,'gamey', {
 var cursors,
     wab,
     gameText,
-    player;
+    player,
+    thrustSpeed,
+    alienBall;
 
 
 
@@ -22,32 +24,51 @@ function preload(){
     game.load.image('wab','./resources/wabbit.png');
     game.load.image('ship','./resources/thrust_ship.png');
     game.load.image('bomb','./resources/xenon2_bomb.png');
+    game.load.image('ball','./resources/pangball.png');
 
 }
 
 function create(){
   game.physics.startSystem(Phaser.Physics.ARCADE);
-    gameText = game.add.text(10,10,'heres text',{font:'bold 14px arial'});
+
+    game.add.sprite(0,0,'sky');
+    scoreText = game.add.text(10,10,'score:',{font:'bold 14px arial'});
 
     player = game.add.sprite(50,200,'ship');
-    wab = game.add.sprite(10,10,'wab');
+  //  wab = game.add.sprite(10,10,'wab');
 
-    game.physics.enable(wab,Phaser.Physics.ARCADE);
+   // game.physics.enable(wab,Phaser.Physics.ARCADE);
 
     game.physics.arcade.enable(player);
-     wab.body.collideWorldBounds = true;
+    // wab.body.collideWorldBounds = true;
 
     player.body.collideWorldBounds = true;
+
+    //enemies
+    alienBall = game.add.group();
+    game.physics.arcade.enable(alienBall);
+    for(var i = 0; i < 5; i++){
+        alienBall.create(game.world.randomX,game.world.randomY,'ball');
+    }
 
      cursors = game.input.keyboard.createCursorKeys();
 
 }
 
 function update(){
+    thrustSpeed = 75;
   if(cursors.left.isDown){
-      player.body.velocity.x = - 50;
+      player.body.velocity.x = - thrustSpeed;
   }
     if(cursors.right.isDown){
-        player.body.velocity.x = + 50;
+        player.body.velocity.x = + thrustSpeed;
+    }
+    if(cursors.down.isDown){
+        player.body.velocity.y = thrustSpeed;
+
+    }
+
+    if(cursors.up.isDown){
+        player.body.velocity.y = - thrustSpeed;
     }
 }
