be06ad5643217b9bc6099ba416851bf72c2f8cda
paul <me@me.com>
2015-06-11 20:26:53 +0100

    added sprites and some movement

--- game.js
+++ game.js
@@ -3,22 +3,51 @@
  * Created by j on 6/11/2015.
  */
 
-var Phaser = require('phaser');
 
 
-var game = new Phaser.game(800,600,Phaser.Auto,'', {
+
+var game = new Phaser.Game(800,600,Phaser.Auto,'gamey', {
     preload:preload,create:create,update:update});
+
+var cursors,
+    wab,
+    gameText,
+    player;
 
 
 
 function preload(){
+    //game.Stage.backgroundColor('#F0000');
+    game.load.image('sky','./resources/sky1.png');
+    game.load.image('wab','./resources/wabbit.png');
+    game.load.image('ship','./resources/thrust_ship.png');
+    game.load.image('bomb','./resources/xenon2_bomb.png');
 
 }
 
 function create(){
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+    gameText = game.add.text(10,10,'heres text',{font:'bold 14px arial'});
+
+    player = game.add.sprite(50,200,'ship');
+    wab = game.add.sprite(10,10,'wab');
+
+    game.physics.enable(wab,Phaser.Physics.ARCADE);
+
+    game.physics.arcade.enable(player);
+     wab.body.collideWorldBounds = true;
+
+    player.body.collideWorldBounds = true;
+
+     cursors = game.input.keyboard.createCursorKeys();
 
 }
 
 function update(){
-
+  if(cursors.left.isDown){
+      player.body.velocity.x = - 50;
+  }
+    if(cursors.right.isDown){
+        player.body.velocity.x = + 50;
+    }
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,20 @@
 /**
+ *
  * Created by j on 6/11/2015.
  */
+
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    connect = require('gulp-connect');
+
+
+gulp.task('brows', function(){
+    return browserify('./game.js')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./dist'));
+
+})
+
+gulp.task('default',['brows']);
