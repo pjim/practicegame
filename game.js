f11d8bdeda12ffd1bda129cd18f91230e089bc60
paul <me@me.com>
2015-06-11 11:23:22 +0100

    phaser boilerplate

--- game.js
+++ game.js
@@ -1,3 +1,24 @@
 /**
+ *
  * Created by j on 6/11/2015.
  */
+
+var Phaser = require('phaser');
+
+
+var game = new Phaser.game(800,600,Phaser.Auto,'', {
+    preload:preload,create:create,update:update});
+
+
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
